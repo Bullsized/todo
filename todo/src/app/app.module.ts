40364ba38f24a5a80804fbb8a4c491f66ca5fcec
Ivan Stefanov <ivanstefanovbg@gmail.com>
2022-02-12 14:11:19 +0200

    adds project structure

--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './features/header/header.component';
+import { FooterComponent } from './features/footer/footer.component';
+import { TodoListComponent } from './features/todos/todo-list/todo-list.component';
+import { TodoAddComponent } from './features/todos/todo-add/todo-add.component';
+import { TodoEditComponent } from './features/todos/todo-edit/todo-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TodoListComponent,
+    TodoAddComponent,
+    TodoEditComponent
   ],
   imports: [
     BrowserModule,
