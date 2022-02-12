b8647968a77f1385dc3711187accabc7a4cbab55
Ivan Stefanov <ivanstefanovbg@gmail.com>
2022-02-12 14:10:42 +0200

    adds service requests & interceptor

--- todo/src/app/core/services/todos.service.ts
+++ todo/src/app/core/services/todos.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Todo } from '../interfaces/todo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodosService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getAllTodos() {
+    return this.http.get<Todo[]>(`${this.baseUrl}`);
+  }
+
+  getTodoById(todoId: string) {
+    return this.http.get<Todo>(`${this.baseUrl}/${todoId}`);
+  }
+
+  postTodo(requestBody: Todo) {
+    return this.http.post<Todo>(`${this.baseUrl}`, requestBody);
+  }
+
+  editTodo(todoId: string, requestBody: Todo) {
+    return this.http.put<Todo>(`${this.baseUrl}/${todoId}`, requestBody);
+  }
+
+  deleteTodo(todoId: string) {
+    return this.http.delete<null>(`${this.baseUrl}/${todoId}`);
+  }
+
+  private get baseUrl() {
+    return `${environment.apiDomain}`;
+  }
 }
--- todo/src/environments/environment.ts
+++ todo/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiDomain: 'https://auto.loanvantage360.com/fps/api/'
 };
 
 /*
